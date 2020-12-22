7d086d2a28a2eb3eac8e3bff72ce8f9384fc66ca
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-12-21 19:56:33 -0700

    Create component from CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component'; // TypeScript needs to know where to find this new component.  Don't put .ts
 import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warningAlert/warningAlert.component'
+import { WarningAlertComponent } from './warningAlert/warningAlert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent, // In Angular, new components are declared here 
     ServersComponent,
-    WarningAlertComponent
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
