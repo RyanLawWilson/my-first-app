16111f141debe5f7731b7c144bed13bd08edc7cf
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-12-21 18:23:40 -0700

    Create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component'; // TypeScript needs to know where to find this new component.  Don't put .ts
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warningAlert/warningAlert.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent, // In Angular, new components are declared here 
-    ServersComponent
+    ServersComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/warningAlert/warningAlert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "warning",
+    templateUrl: "./warningAlert.component.html",
+    styleUrls: ["./warningAlert.component.css"]
+})
+export class WarningAlertComponent {
+
+}
