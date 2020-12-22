d3afd1d3559de612bc153bbde23b06105aaa21dd
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-12-21 20:39:16 -0700

    Apply Bootstrap classes

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '.app-servers', // Can select by attribute and class.  NOT by id and NOT by pseudo selectors
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  selector: 'app-servers', // Can select by attribute and class.  NOT by id and NOT by pseudo selectors
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
